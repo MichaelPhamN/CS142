21916941166e078e94fe9b55337852efd165bded
ppham02219 <ppham02219@B16-IC-U3.titan.tccnet.edu>
2017-01-18 11:01:59 -0800

    add code on Baking Calculator

--- Assignment1/src/BakingCalculator.java
+++ Assignment1/src/BakingCalculator.java
@@ -5,61 +5,89 @@ public class BakingCalculator {
 		int cookieCount = 24;
 		int loafCount = 1;
 		
-		double recipeCookieDozen = 4;
-		
+		//================================ recipe and change value of cookie
+		double recipeCookieDozen = 4;		
 		double recipeCookieAllPurposeFlourCup = 9.0/4.0;
 		//Change Cup to Lb of flour
-		double recipeCookieAllPurposeFlourLb = (double)recipeCookieAllPurposeFlourCup * 3.0/10.0;	
+		double recipeCookieAllPurposeFlourLb = (double)recipeCookieAllPurposeFlourCup * 3.0/10.0;
 		
 		double recipeCookieBakingSodaTeaspoon = 1;
 		//Change teaspoon to oz of baking soda
-		double recipeCookieBakingSodaOz = (double)recipeCookieBakingSodaTeaspoon/6;		
+		double recipeCookieBakingSodaOz = (double)recipeCookieBakingSodaTeaspoon/6;
+		
 		
 		double recipeCookieSaltTeaspoon = 1;
 		//Change teaspoon to oz of Salt
 		double recipeCookieSaltOz = (double)recipeCookieSaltTeaspoon/6;
 		
+		
 		double recipeCookieButterCup = 1;
 		//Change cup to lb of butter
-		double recipeCookieButterOz = (double)recipeCookieButterCup/2;
+		double recipeCookieButterLb = (double)recipeCookieButterCup/2;
+		
 		
 		double recipeCookieSugarCup = 3.0/2.0;
-		//Change teaspoon to Lb of Sugar
+		//Change Cup to Lb of Sugar
 		double recipeCookieSugarLb = (double)recipeCookieSugarCup/2;
+		
 		
 		double recipeCookieVanillaExtractTeaspoon = 1;
 		//Change teaspoon to a liquid ounce of VanillaExtract
 		double recipeCookieVanillaExtractOz = (double)recipeCookieVanillaExtractTeaspoon/6;
 		
+		
 		double recipeCookieEgg = 2;		
 		double recipeCookieChocolateChipCup = 5.0/2.0;
-		
+
+		//================================ recipe and change value of a loaf of banana bread
 		double recipeBananaBreadLoaf = 1;
 		double recipeBananaBreadAllPurposeFlourCup = 3.0/2.0;
 		//Change cup to Lb of flour
-		double recipeBananaBreadAllPurposeFlourLb = recipeBananaBreadAllPurposeFlourCup* 3.0/10.0;
+		double recipeBananaBreadAllPurposeFlourLb = (double)recipeBananaBreadAllPurposeFlourCup * 3.0/10.0;
+		
 		
 		double recipeBananaBreadBakingSodaTeaspoon = 1;
-		//Change cup to Lb of flour
+		//Change teaspoon to oz of baking soda
+		double recipeBananaBreadBakingSodaOz = (double)recipeBananaBreadBakingSodaTeaspoon/6;
 		
-		//Change cup to Lb of flour
-		double recipeBananaSaltTeaspoon = 1/8;
+				
+		double recipeBananaBreadSaltTeaspoon = 1.0/8.0;
+		//Change teaspoon to oz of Salt
+		double recipeBananaBreadSaltOz = (double)recipeBananaBreadSaltTeaspoon/6;		
 		
-		//Change cup to Lb of flour
-		double recipeBananaBreadButterCup = 1/3;
+		double recipeBananaBreadButterCup = 1.0/3.0;
+		//Change cup to lb of butter
+		double recipeBananaBreadButterLb = (double)recipeBananaBreadButterCup/2;
 		
-		//Change cup to Lb of flour
-		double recipeBananaBreadSugarCup = 3/4;
 		
-		//Change cup to Lb of flour
+		double recipeBananaBreadSugarCup = 3.0/4.0;
+		//Change Cup to Lb of Sugar
+		double recipeBananaBreadSugarLb = (double)recipeBananaBreadSugarCup/2;
+		
+				
 		double recipeBananaBreadVanillaExtractTeaspoon = 1;
+		//Change teaspoon to a liquid ounce of VanillaExtract
+		double recipeBananaBreadVanillaExtractOz = (double)recipeBananaBreadVanillaExtractTeaspoon/6;
+		
 		
 		double recipeBananaBreadEgg = 1;
 		double recipeBananaBreadBanana = 2;
 		
 		
 		double scaleCookie = (double)cookieCount/(recipeCookieDozen * 12);
+		//============================ ingredients for x cookieCount
+		double desiredCookieAllPurposeFlourLb = scaleCookie * recipeCookieAllPurposeFlourLb;
+		double desiredCookieBakingSodaOz = scaleCookie * recipeCookieBakingSodaOz;		
+		double desiredCookieSaltOz = scaleCookie * recipeCookieSaltOz;		
+		double desiredCookieButterLb = scaleCookie * recipeCookieButterLb;		
+		double desiredCookieSugarLb = scaleCookie * recipeCookieSugarLb;		
+		double desiredCookieVanillaExtractOz = scaleCookie * recipeCookieVanillaExtractOz;		
+		double desiredCookieEgg = scaleCookie * recipeCookieEgg;		
+		double desiredCookieChocolateChipCup = scaleCookie * recipeCookieChocolateChipCup;
+		
+		
 		double scaleBananaBread = (double)loafCount/recipeBananaBreadLoaf;
+		//============================ ingredients for x loaf of Banana Bread
 		
 		
 	}
