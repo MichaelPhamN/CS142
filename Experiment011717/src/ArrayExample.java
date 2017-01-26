81f4642c562f1348892d56caef2d47228ae58c08
ppham02219 <michaelphamn@gmail.com>
2017-01-26 10:21:55 -0800

    asdsad

---
+++ Experiment011717/src/ArrayExample.java
@@ -0,0 +1,11 @@
+
+public class ArrayExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a= {1,2,3,4,5,6};
+		for(int i = 0; i < a.length; i++)			
+			System.out.println("Element " + (i + 1) + " th = " + a[i]);
+	}
+
+}
