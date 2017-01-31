0392ef0eeb9b449b4fe81fda9ca69978162dca29
Admin <Admin@10.0.0.6>
2017-01-30 22:09:05 -0800

    Assignment three

--- AssignmentThree/src/NumericalIntegrator.java
+++ AssignmentThree/src/NumericalIntegrator.java
@@ -4,8 +4,7 @@ public class NumericalIntegrator {
 	public static void main(String[] args) {
 		double minX = -2.0;
 		double maxX = 2.0;		
-		int numPoints = 3;
-		
+		int numPoints = 3;		
 		double rangeInterval = (maxX - minX)/(numPoints - 1);
 		double sumAreas = 0;
 		for(double i = minX; i  < maxX; i = i + rangeInterval){
