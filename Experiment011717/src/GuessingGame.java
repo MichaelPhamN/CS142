022a0fefda24e3097e1f37b5667b5f73bd9ec938
ppham02219 <michaelphamn@gmail.com>
2017-02-07 10:21:18 -0800

    Assignmen 4

--- Experiment011717/src/GuessingGame.java
+++ Experiment011717/src/GuessingGame.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class GuessingGame {
 	public static int[] generateSecretCode(int length, int numberOfSymbols){
@@ -9,11 +10,44 @@ public class GuessingGame {
 		
 		return secretCode;
 	}
-	public static void main(String[] args) {
-		
-			System.out.println(Arrays.toString(generateSecretCode(4,6)));
-		
+	
+	public static int numCorrectDigitsPosition(int[] code, int[] guess){
+		//Run through the two arrays
+		//If they have the same element in the same position, that's one more correct digit.
+		int count = 0;
+		for(int i = 0; i < code.length; i++){			
+			if(code[i] == guess[i]){
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	public static void main(String[] args) {		
+		int numColors = 6;
+		int length = 4;
+		System.out.println(Arrays.toString(generateSecretCode(4,6)));
+		Scanner s = new Scanner(System.in);
+		System.out.println("Please enter a guess (" + length + " digits 1-" + numColors + ")");
+		String input = s.nextLine();
+		if(input.length() == length){
+			char[] characters = input.toCharArray();
+			int[] guess = new int[length];
+			boolean isValid = true;
+			for(int i = 0; i< length; i++){
+				if(characters[i] >= '1' && characters[i] < '0'+numColors){
 
+				}else{
+					System.out.println("Character " + characters[i] + " is not a valid code digit !!!");
+					isValid = true;
+				}
+			}
+			if(isValid){
+				
+			}
+		}else{
+			System.out.println("Your guess was not the correct length");
+		}
 	}
 
 }
