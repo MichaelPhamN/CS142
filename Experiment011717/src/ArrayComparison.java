6ffde83e6819fe278885520afc332f954e4108bf
ppham02219 <michaelphamn@gmail.com>
2017-02-10 10:10:38 -0800

    10/02/17

---
+++ Experiment011717/src/ArrayComparison.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+public class ArrayComparison {
+	public static boolean equals(int[] a, int[] b){		
+		if(a == b) return true;
+		if(a == null || b == null) return false;
+		// check length of array A and B
+		if(a.length != b.length)
+			return false;
+		
+		// compare values in arrays
+		for(int i=0; i< a.length; i++)
+			if(a[i] != b[i])			
+				return false;
+		return true;
+	}
+	
+	public static boolean equals(boolean[] a, boolean[] b){
+		if(a == b) return true;
+		if(a == null || b == null) return false;
+		// check length of array A and B
+		if(a.length != b.length)
+			return false;
+		
+		// compare values in arrays
+		for(int i=0; i< a.length; i++)
+			if(a[i] != b[i])			
+				return false;
+		return true;
+	}
+	public static void main(String[] args) {
+		boolean[] a = {true,false,true};
+		boolean[] b = {true,false,true};
+		
+//		if(Arrays.equals(a, b)){
+//			System.out.println("A Equal B");
+//		}else{
+//			System.out.println("Not Equal");			
+//		}
+		
+		if(equals(a, b)){
+			System.out.println("A Equal B");
+		}else{
+			System.out.println("Not Equal");			
+		}
+	}
+
+}
