3c3f291539cded605044fe02bf03b7e5fb957dff
ppham02219 <michaelphamn@gmail.com>
2017-02-13 10:21:16 -0800

    hjkh

--- Experiment011717/src/ArraysAndChars.java
+++ Experiment011717/src/ArraysAndChars.java
@@ -9,11 +9,29 @@ public class ArraysAndChars {
 		}
 		return revStr;
 	}
+	
+	public static boolean isPalindrome(String s){
+		char[] c = s.toCharArray();
+		for(int i = 0; i < c.length/2; i++){
+			if(c[i] != c[c.length - i - 1])
+				return false;
+		}
+		return true;
+//		if(s.equals(reverse(s))) return true; return false;
+		/*
+		 * for(int i = 0; i < s.length(); i++){
+		 * 		if(s.charAt(i) != s.charAt(s.length() - i - 1))
+		 * 			return false
+		 * return true;
+		 * }
+		 */
+	}
+	
 	public static void main(String[] args){		
 		String s = "";
-		s = "Hi, how are you?";
+		s = "racecar";		
 		System.out.println(reverse(s));
-		
+		System.out.println(isPalindrome(s));
 	}
 
 }
