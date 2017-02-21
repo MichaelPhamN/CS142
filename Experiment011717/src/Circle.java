9b4c86e2ec11228946461ffde708191f022a91fd
ppham02219 <michaelphamn@gmail.com>
2017-02-21 10:22:36 -0800

    ds

--- Experiment011717/src/Circle.java
+++ Experiment011717/src/Circle.java
@@ -1,10 +1,29 @@
 
 public class Circle {
-	double radius;
-	double CenterX, CenterY;
+	public double radius;
+	public double centerX, centerY;
 	public Circle(double CenterX, double CenterY, double radius){
 		this.radius = radius;
-		this.CenterX = CenterX;
-		this.CenterY = CenterY;
+		this.centerX = CenterX;
+		this.centerY = CenterY;
+	}
+	// String representation: ({x}, {y}) radius {r}
+	// where {x},{y}, {r} are the actual values
+	public String toString(){
+		return "({" + this.centerX + "}, {" + this.centerY + "}) radius {" + this.radius + "}";
+	}
+	
+	public boolean equals(Object o){
+		if(o instanceof Circle){
+			Circle c = (Circle) o;
+			return centerX == c.centerX && centerY== c.centerY && radius == c.radius;
+		}
+		return false;
+	}
+	public boolean intersects(Circle c){
+		// compute the distance between the two centers
+		// square root of (x1 - x2)^2 + (y1 - y2)^2
+		return (centerX - c.centerX)*(centerX - c.centerX) 
+				+ (centerY - c.centerY)*(centerY - c.centerY) < radius + c.radius;
 	}
 }
---
+++ Experiment011717/src/CircleTest.java
@@ -0,0 +1,16 @@
+
+public class CircleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Circle c1 = new Circle(0,0,3);
+		System.out.println(c1);
+		Circle c2 = new Circle(0,0,3);
+		System.out.println(c1.equals(c2));
+	}
+	
+	// return true if the cirlces are touching or intersect
+	// false if they don't touch at all
+	
+
+}
