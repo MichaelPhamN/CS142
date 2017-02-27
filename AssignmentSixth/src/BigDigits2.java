12c852512f38d8a8cf3eea797561cf25a8cc874a
Admin <Admin@10.0.0.2>
2017-02-27 06:56:09 -0800

    dsf

--- AssignmentSixth/src/BigDigits2.java
+++ AssignmentSixth/src/BigDigits2.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
  * Author: Phuc Pham N
  *
  */
-public class BigDigits {
+public class BigDigits2 {
 	
 	public byte[] arrDigits;
 	
@@ -25,14 +25,14 @@ public class BigDigits {
 	/*
 	 * Initial BigDigits 
 	 */
-	public BigDigits(){
+	public BigDigits2(){
 		
 	}
 	
 	/*
 	 * Initial BigDigits 
 	 */
-	public BigDigits(String number) {		
+	public BigDigits2(String number) {		
 		if(number.length() == 0)
 			this.arrDigits[0] = 0;
 		else{		
@@ -61,45 +61,39 @@ public class BigDigits {
 	 * the BigDigits objects. Instead, create and return a new BigDigits object
 	 * representing the sum.
 	 */
-	public BigDigits plus(BigDigits d) {		
+	public BigDigits2 plus(BigDigits2 d) {		
 		if(this.arrDigits.length > d.arrDigits.length){
 			byte[] tmp = new byte[this.arrDigits.length];			
-			if(d.arrDigits[0] == 0 && this.arrDigits[0] == 0){	
-				for (int i = 0; i < d.arrDigits.length; i++) {					
-					tmp[this.arrDigits.length - i - 1] = d.arrDigits[d.arrDigits.length - i - 1];					
-				}
-				for (int i = 0; i < this.arrDigits.length; i++) {
-					this.arrDigits[this.arrDigits.length - i - 1] = (byte)(this.arrDigits[this.arrDigits.length - i - 1] + tmp[this.arrDigits.length - i - 1]);					
-					if(this.arrDigits[this.arrDigits.length - i - 1] > 9){												
-						this.arrDigits[this.arrDigits.length - i - 2] = (byte) (this.arrDigits[this.arrDigits.length - i - 2] + 1);
-						this.arrDigits[this.arrDigits.length - i - 1] = (byte) (this.arrDigits[this.arrDigits.length - i - 1]%10);						
-					}
-				}											
-			}else if(d.arrDigits[0] == -1 && this.arrDigits[0] == 0){
+			if(d.arrDigits[0] == -1){				
 				tmp[0] = -1;
 				for (int i = 1; i < d.arrDigits.length; i++) {					
 					if((i+1) != d.arrDigits.length){
-						tmp[this.arrDigits.length - i] = d.arrDigits[d.arrDigits.length - i];
+						tmp[this.arrDigits.length - i] = d.arrDigits[d.arrDigits.length - i - 1];
 					}					
 				}
-				for (int i = 0; i < this.arrDigits.length - 1; i++) {					
-					if(this.arrDigits[this.arrDigits.length - i - 1] >= tmp[this.arrDigits.length - i - 1]){
-						this.arrDigits[this.arrDigits.length - i - 1] = (byte)(this.arrDigits[this.arrDigits.length - i - 1] - tmp[this.arrDigits.length - i - 1]);
-					}else{	
-						this.arrDigits[this.arrDigits.length - i - 2] = (byte) (this.arrDigits[this.arrDigits.length - i - 2] - 1);
-						this.arrDigits[this.arrDigits.length - i - 1] = (byte) ((this.arrDigits[this.arrDigits.length - i - 1] + 10) - tmp[this.arrDigits.length - i - 1]);
-					}
+			}else{
+				for (int i = 0; i < d.arrDigits.length; i++) {					
+					tmp[this.arrDigits.length - i - 1] = d.arrDigits[d.arrDigits.length - i - 1];					
 				}
-			}else if(d.arrDigits[0] == 0 && this.arrDigits[0] == -1){
+//				byte[] arrSum = new byte[this.arrDigits.length];
+//				System.err.println(Arrays.toString(this.arrDigits));
+//				System.out.println(Arrays.toString(tmp));
+				for (int i = 0; i < this.arrDigits.length; i++) {
+//					System.out.print(this.arrDigits[this.arrDigits.length - i - 1]);
+//					System.err.print(tmp[this.arrDigits.length - i - 1]);
+					this.arrDigits[this.arrDigits.length - i - 1] = (byte)(this.arrDigits[this.arrDigits.length - i - 1] + tmp[this.arrDigits.length - i - 1]);
+//					System.out.println(this.arrDigits[this.arrDigits.length - i - 1]);					
+					if(this.arrDigits[this.arrDigits.length - i - 1] > 9){												
+						this.arrDigits[this.arrDigits.length - i - 2] = (byte) (this.arrDigits[this.arrDigits.length - i - 2] + 1);
+						this.arrDigits[this.arrDigits.length - i - 1] = (byte) (this.arrDigits[this.arrDigits.length - i - 1]%10);
+						
+					}
+				}				
 				
-			}
-			
-		}else if(this.arrDigits.length < d.arrDigits.length){
-			
-		}else if(this.arrDigits.length == d.arrDigits.length){
+			}			
 			
 		}
-		System.out.println(Arrays.toString(this.arrDigits));
+//		System.out.println(Arrays.toString(this.arrDigits));
 		System.exit(0);
 		return this;
 	}
@@ -131,7 +125,7 @@ public class BigDigits {
 	 * 
 	 * @return -1, 0, or 1
 	 */
-	public byte compareTo(BigDigits d) {
+	public byte compareTo(BigDigits2 d) {
 		// FIXME This method should only return 0 if this is equal to d!
 		return 0;
 	}
@@ -140,7 +134,7 @@ public class BigDigits {
 	 * EXTRA CREDIT 5 POINTS: Compute the product of two BigDigits numbers. As
 	 * with plus, don't modify this or d, create a new BigDigits object.
 	 */
-	public BigDigits times(BigDigits d) {
+	public BigDigits2 times(BigDigits2 d) {
 		// FIXME This method should never return null if you're implementing the
 		// extra credit!
 		return null;
