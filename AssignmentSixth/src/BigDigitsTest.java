cc8a7849ca3b6afb0710f7b049973783052d24a8
ppham02219 <michaelphamn@gmail.com>
2017-02-28 08:00:10 -0800

    Test Again

--- AssignmentSixth/src/BigDigitsTest.java
+++ AssignmentSixth/src/BigDigitsTest.java
@@ -5,8 +5,8 @@ public class BigDigitsTest {
 	public static void main(String[] args) {
 		
 		// TODO Auto-generated method stub
-		BigDigits a = new BigDigits("998");
-		BigDigits b = new BigDigits("-999");
+		BigDigits a = new BigDigits("-11111111111");
+		BigDigits b = new BigDigits("1111111");
 		byte[] tmp = new byte[10];
 //		System.out.println(Arrays.toString(tmp));
 		BigDigits c = a.plus(b);
