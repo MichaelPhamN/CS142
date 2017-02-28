e562a987c1cf002b516d22d003bdc99361ca5a41
Admin <Admin@10.0.0.2>
2017-02-28 00:44:35 -0800

    a

--- AssignmentSixth/src/BigDigits.java
+++ AssignmentSixth/src/BigDigits.java
@@ -61,165 +61,192 @@ public class BigDigits {
 	 * the BigDigits objects. Instead, create and return a new BigDigits object
 	 * representing the sum.
 	 */
-	public BigDigits plus(BigDigits d) {		
-		if(this.arrDigits.length > d.arrDigits.length){			
-			byte[] tmp = new byte[this.arrDigits.length];			
-			if(d.arrDigits[0] == 0 && this.arrDigits[0] == 0){	
+	public BigDigits plus(BigDigits d) {
+		BigDigits b = new BigDigits();
+				
+		if(this.arrDigits.length > d.arrDigits.length){
+			// copy this
+			b.arrDigits = new byte[this.arrDigits.length];	
+			for (int i = 0; i < this.arrDigits.length; i++) {
+				b.arrDigits[i] = this.arrDigits[i];
+			}
+			byte[] tmp = new byte[b.arrDigits.length];
+			if(d.arrDigits[0] == 0 && b.arrDigits[0] == 0){
 				for (int i = 0; i < d.arrDigits.length; i++) {					
-					tmp[this.arrDigits.length - i - 1] = d.arrDigits[d.arrDigits.length - i - 1];					
+					tmp[b.arrDigits.length - i - 1] = d.arrDigits[d.arrDigits.length - i - 1];					
 				}
-				for (int i = 0; i < this.arrDigits.length; i++) {
-					this.arrDigits[this.arrDigits.length - i - 1] = (byte)(this.arrDigits[this.arrDigits.length - i - 1] + tmp[this.arrDigits.length - i - 1]);					
-					if(this.arrDigits[this.arrDigits.length - i - 1] > 9){												
-						this.arrDigits[this.arrDigits.length - i - 2] = (byte) (this.arrDigits[this.arrDigits.length - i - 2] + 1);
-						this.arrDigits[this.arrDigits.length - i - 1] = (byte) (this.arrDigits[this.arrDigits.length - i - 1]%10);						
+				for (int i = 0; i < b.arrDigits.length; i++) {
+					b.arrDigits[b.arrDigits.length - i - 1] = (byte)(b.arrDigits[b.arrDigits.length - i - 1] + tmp[b.arrDigits.length - i - 1]);					
+					if(b.arrDigits[b.arrDigits.length - i - 1] > 9){												
+						b.arrDigits[b.arrDigits.length - i - 2] = (byte) (b.arrDigits[b.arrDigits.length - i - 2] + 1);
+						b.arrDigits[b.arrDigits.length - i - 1] = (byte) (b.arrDigits[b.arrDigits.length - i - 1]%10);						
 					}
 				}											
-			}else if(d.arrDigits[0] == -1 && this.arrDigits[0] == 0){
+			}else if(d.arrDigits[0] == -1 && b.arrDigits[0] == 0){
 				tmp[0] = -1;
 				for (int i = 1; i < d.arrDigits.length; i++) {					
 					if((i+1) != d.arrDigits.length){
-						tmp[this.arrDigits.length - i] = d.arrDigits[d.arrDigits.length - i];
+						tmp[b.arrDigits.length - i] = d.arrDigits[d.arrDigits.length - i];
 					}					
 				}
-				for (int i = 0; i < this.arrDigits.length - 1; i++) {					
-					if(this.arrDigits[this.arrDigits.length - i - 1] >= tmp[this.arrDigits.length - i - 1]){
-						this.arrDigits[this.arrDigits.length - i - 1] = (byte)(this.arrDigits[this.arrDigits.length - i - 1] - tmp[this.arrDigits.length - i - 1]);
+				for (int i = 0; i < b.arrDigits.length - 1; i++) {					
+					if(b.arrDigits[b.arrDigits.length - i - 1] >= tmp[b.arrDigits.length - i - 1]){
+						b.arrDigits[b.arrDigits.length - i - 1] = (byte)(b.arrDigits[b.arrDigits.length - i - 1] - tmp[b.arrDigits.length - i - 1]);
 					}else{	
-						this.arrDigits[this.arrDigits.length - i - 2] = (byte) (this.arrDigits[this.arrDigits.length - i - 2] - 1);
-						this.arrDigits[this.arrDigits.length - i - 1] = (byte) ((this.arrDigits[this.arrDigits.length - i - 1] + 10) - tmp[this.arrDigits.length - i - 1]);
+						b.arrDigits[b.arrDigits.length - i - 2] = (byte) (b.arrDigits[b.arrDigits.length - i - 2] - 1);
+						b.arrDigits[b.arrDigits.length - i - 1] = (byte) ((b.arrDigits[b.arrDigits.length - i - 1] + 10) - tmp[b.arrDigits.length - i - 1]);
 					}
 				}
-			}else if(d.arrDigits[0] == 0 && this.arrDigits[0] == -1){							
+			}else if(d.arrDigits[0] == 0 && b.arrDigits[0] == -1){							
 				for (int i = 0; i < d.arrDigits.length; i++) {					
-					tmp[this.arrDigits.length - i - 1] = d.arrDigits[d.arrDigits.length - i - 1];					
+					tmp[b.arrDigits.length - i - 1] = d.arrDigits[d.arrDigits.length - i - 1];					
 				}
-				for (int i = 0; i < this.arrDigits.length-1; i++) {					
-					if(this.arrDigits[this.arrDigits.length - i - 1] >= tmp[this.arrDigits.length - i - 1]){
-						this.arrDigits[this.arrDigits.length - i - 1] = (byte)(this.arrDigits[this.arrDigits.length - i - 1] - tmp[this.arrDigits.length - i - 1]);
+				for (int i = 0; i < b.arrDigits.length-1; i++) {					
+					if(b.arrDigits[b.arrDigits.length - i - 1] >= tmp[b.arrDigits.length - i - 1]){
+						b.arrDigits[b.arrDigits.length - i - 1] = (byte)(b.arrDigits[b.arrDigits.length - i - 1] - tmp[b.arrDigits.length - i - 1]);
 					}else{	
-						this.arrDigits[this.arrDigits.length - i - 2] = (byte) (this.arrDigits[this.arrDigits.length - i - 2] - 1);
-						this.arrDigits[this.arrDigits.length - i - 1] = (byte) ((this.arrDigits[this.arrDigits.length - i - 1] + 10) - tmp[this.arrDigits.length - i - 1]);
+						b.arrDigits[b.arrDigits.length - i - 2] = (byte) (b.arrDigits[b.arrDigits.length - i - 2] - 1);
+						b.arrDigits[b.arrDigits.length - i - 1] = (byte) ((b.arrDigits[b.arrDigits.length - i - 1] + 10) - tmp[b.arrDigits.length - i - 1]);
 					}
 				}
-			}else if(d.arrDigits[0] == -1 && this.arrDigits[0] == -1){
+			}else if(d.arrDigits[0] == -1 && b.arrDigits[0] == -1){
 				tmp[0] = -1;
 				for (int i = 1; i < d.arrDigits.length; i++) {					
 					if((i+1) != d.arrDigits.length){
-						tmp[this.arrDigits.length - i] = d.arrDigits[d.arrDigits.length - i];
+						tmp[b.arrDigits.length - i] = d.arrDigits[d.arrDigits.length - i];
 					}					
 				}
-				for (int i = 0; i < this.arrDigits.length-1; i++) {
-					this.arrDigits[this.arrDigits.length - i - 1] = (byte)(this.arrDigits[this.arrDigits.length - i - 1] + tmp[this.arrDigits.length - i - 1]);					
-					if(this.arrDigits[this.arrDigits.length - i - 1] > 9){												
-						this.arrDigits[this.arrDigits.length - i - 2] = (byte) (this.arrDigits[this.arrDigits.length - i - 2] + 1);
-						this.arrDigits[this.arrDigits.length - i - 1] = (byte) (this.arrDigits[this.arrDigits.length - i - 1]%10);						
+				for (int i = 0; i < b.arrDigits.length-1; i++) {
+					b.arrDigits[b.arrDigits.length - i - 1] = (byte)(b.arrDigits[b.arrDigits.length - i - 1] + tmp[b.arrDigits.length - i - 1]);					
+					if(b.arrDigits[b.arrDigits.length - i - 1] > 9){												
+						b.arrDigits[b.arrDigits.length - i - 2] = (byte) (b.arrDigits[b.arrDigits.length - i - 2] + 1);
+						b.arrDigits[b.arrDigits.length - i - 1] = (byte) (b.arrDigits[b.arrDigits.length - i - 1]%10);						
 					}
 				}			
-			}	
-//			System.out.println(Arrays.toString(this.arrDigits));
-//			System.exit(0);
-			return this;
+			}			
 		}else if(this.arrDigits.length < d.arrDigits.length){
-			//Check lai cho nay
-			byte[] tmp = new byte[d.arrDigits.length];
-			if(this.arrDigits[0] == 0 && d.arrDigits[0] == 0){	
+			// copy d
+			b.arrDigits = new byte[d.arrDigits.length];	
+			for (int i = 0; i < d.arrDigits.length; i++) {
+				b.arrDigits[i] = d.arrDigits[i];
+			}
+			
+			byte[] tmp = new byte[b.arrDigits.length];
+			if(this.arrDigits[0] == 0 && b.arrDigits[0] == 0){	
 				for (int i = 0; i < this.arrDigits.length; i++) {					
-					tmp[d.arrDigits.length - i - 1] = this.arrDigits[this.arrDigits.length - i - 1];					
+					tmp[b.arrDigits.length - i - 1] = this.arrDigits[this.arrDigits.length - i - 1];					
 				}
-				for (int i = 0; i < d.arrDigits.length; i++) {
-					d.arrDigits[d.arrDigits.length - i - 1] = (byte)(d.arrDigits[d.arrDigits.length - i - 1] + tmp[d.arrDigits.length - i - 1]);					
-					if(d.arrDigits[d.arrDigits.length - i - 1] > 9){												
-						d.arrDigits[d.arrDigits.length - i - 2] = (byte) (d.arrDigits[d.arrDigits.length - i - 2] + 1);
-						d.arrDigits[d.arrDigits.length - i - 1] = (byte) (d.arrDigits[d.arrDigits.length - i - 1]%10);						
+				for (int i = 0; i < b.arrDigits.length; i++) {
+					b.arrDigits[d.arrDigits.length - i - 1] = (byte)(b.arrDigits[b.arrDigits.length - i - 1] + tmp[b.arrDigits.length - i - 1]);					
+					if(b.arrDigits[b.arrDigits.length - i - 1] > 9){												
+						b.arrDigits[b.arrDigits.length - i - 2] = (byte) (b.arrDigits[b.arrDigits.length - i - 2] + 1);
+						b.arrDigits[b.arrDigits.length - i - 1] = (byte) (b.arrDigits[b.arrDigits.length - i - 1]%10);						
 					}
 				}											
-			}else if(this.arrDigits[0] == -1 && d.arrDigits[0] == 0){				
+			}else if(this.arrDigits[0] == -1 && b.arrDigits[0] == 0){				
 				tmp[0] = -1;
 				for (int i = 1; i < this.arrDigits.length; i++) {					
 					if((i+1) != this.arrDigits.length){
-						tmp[d.arrDigits.length - i] = this.arrDigits[this.arrDigits.length - i];
+						tmp[b.arrDigits.length - i] = this.arrDigits[this.arrDigits.length - i];
 					}					
 				}
-				for (int i = 0; i < d.arrDigits.length - 1; i++) {					
-					if(d.arrDigits[d.arrDigits.length - i - 1] >= tmp[d.arrDigits.length - i - 1]){
-						d.arrDigits[d.arrDigits.length - i - 1] = (byte)(d.arrDigits[d.arrDigits.length - i - 1] - tmp[d.arrDigits.length - i - 1]);
+				for (int i = 0; i < b.arrDigits.length - 1; i++) {					
+					if(b.arrDigits[b.arrDigits.length - i - 1] >= tmp[b.arrDigits.length - i - 1]){
+						b.arrDigits[b.arrDigits.length - i - 1] = (byte)(b.arrDigits[b.arrDigits.length - i - 1] - tmp[b.arrDigits.length - i - 1]);
 					}else{	
-						d.arrDigits[d.arrDigits.length - i - 2] = (byte) (d.arrDigits[d.arrDigits.length - i - 2] - 1);
-						d.arrDigits[d.arrDigits.length - i - 1] = (byte) ((d.arrDigits[d.arrDigits.length - i - 1] + 10) - tmp[d.arrDigits.length - i - 1]);
+						b.arrDigits[b.arrDigits.length - i - 2] = (byte) (b.arrDigits[b.arrDigits.length - i - 2] - 1);
+						b.arrDigits[b.arrDigits.length - i - 1] = (byte) ((b.arrDigits[b.arrDigits.length - i - 1] + 10) - tmp[b.arrDigits.length - i - 1]);
 					}
 				}
-			}else if(this.arrDigits[0] == 0 && d.arrDigits[0] == -1){							
+			}else if(this.arrDigits[0] == 0 && b.arrDigits[0] == -1){							
 				for (int i = 0; i < this.arrDigits.length; i++) {					
-					tmp[d.arrDigits.length - i - 1] = this.arrDigits[this.arrDigits.length - i - 1];					
+					tmp[b.arrDigits.length - i - 1] = this.arrDigits[this.arrDigits.length - i - 1];					
 				}
-				for (int i = 0; i < d.arrDigits.length-1; i++) {					
-					if(d.arrDigits[d.arrDigits.length - i - 1] >= tmp[d.arrDigits.length - i - 1]){
-						d.arrDigits[d.arrDigits.length - i - 1] = (byte)(d.arrDigits[d.arrDigits.length - i - 1] - tmp[d.arrDigits.length - i - 1]);
+				for (int i = 0; i < b.arrDigits.length-1; i++) {					
+					if(b.arrDigits[b.arrDigits.length - i - 1] >= tmp[b.arrDigits.length - i - 1]){
+						b.arrDigits[b.arrDigits.length - i - 1] = (byte)(b.arrDigits[b.arrDigits.length - i - 1] - tmp[b.arrDigits.length - i - 1]);
 					}else{	
-						d.arrDigits[d.arrDigits.length - i - 2] = (byte) (d.arrDigits[d.arrDigits.length - i - 2] - 1);
-						d.arrDigits[d.arrDigits.length - i - 1] = (byte) ((d.arrDigits[d.arrDigits.length - i - 1] + 10) - tmp[d.arrDigits.length - i - 1]);
+						b.arrDigits[b.arrDigits.length - i - 2] = (byte) (b.arrDigits[b.arrDigits.length - i - 2] - 1);
+						b.arrDigits[b.arrDigits.length - i - 1] = (byte) ((b.arrDigits[b.arrDigits.length - i - 1] + 10) - tmp[b.arrDigits.length - i - 1]);
 					}
 				}
-			}else if(this.arrDigits[0] == -1 && d.arrDigits[0] == -1){
+			}else if(this.arrDigits[0] == -1 && b.arrDigits[0] == -1){
 				tmp[0] = -1;
 				for (int i = 1; i < this.arrDigits.length; i++) {					
 					if((i+1) != this.arrDigits.length){
-						tmp[d.arrDigits.length - i] = this.arrDigits[this.arrDigits.length - i];
+						tmp[b.arrDigits.length - i] = this.arrDigits[this.arrDigits.length - i];
 					}					
 				}
-				for (int i = 0; i < d.arrDigits.length-1; i++) {
-					d.arrDigits[d.arrDigits.length - i - 1] = (byte)(d.arrDigits[d.arrDigits.length - i - 1] + tmp[d.arrDigits.length - i - 1]);					
-					if(d.arrDigits[d.arrDigits.length - i - 1] > 9){												
-						d.arrDigits[d.arrDigits.length - i - 2] = (byte) (d.arrDigits[d.arrDigits.length - i - 2] + 1);
-						d.arrDigits[d.arrDigits.length - i - 1] = (byte) (d.arrDigits[d.arrDigits.length - i - 1]%10);						
+				for (int i = 0; i < b.arrDigits.length-1; i++) {
+					b.arrDigits[b.arrDigits.length - i - 1] = (byte)(b.arrDigits[b.arrDigits.length - i - 1] + tmp[b.arrDigits.length - i - 1]);					
+					if(b.arrDigits[b.arrDigits.length - i - 1] > 9){												
+						b.arrDigits[b.arrDigits.length - i - 2] = (byte) (b.arrDigits[b.arrDigits.length - i - 2] + 1);
+						b.arrDigits[b.arrDigits.length - i - 1] = (byte) (b.arrDigits[b.arrDigits.length - i - 1]%10);						
 					}
 				}			
 			}
-//			System.out.println(Arrays.toString(d.arrDigits));
-//			System.exit(0);
-			return d;
-		}else if(this.arrDigits.length == d.arrDigits.length){					
+		}else if(this.arrDigits.length == d.arrDigits.length){
+			b.arrDigits = new byte[this.arrDigits.length];
 			if(d.arrDigits[0] == 0 && this.arrDigits[0] == 0){
 				for (int i = 0; i < this.arrDigits.length; i++) {
-					this.arrDigits[this.arrDigits.length - i - 1] = (byte)(this.arrDigits[this.arrDigits.length - i - 1] + d.arrDigits[this.arrDigits.length - i - 1]);					
-					if(this.arrDigits[this.arrDigits.length - i - 1] > 9){												
-						this.arrDigits[this.arrDigits.length - i - 2] = (byte) (this.arrDigits[this.arrDigits.length - i - 2] + 1);
-						this.arrDigits[this.arrDigits.length - i - 1] = (byte) (this.arrDigits[this.arrDigits.length - i - 1]%10);						
+					b.arrDigits[this.arrDigits.length - i - 1] = (byte)(this.arrDigits[this.arrDigits.length - i - 1] + d.arrDigits[this.arrDigits.length - i - 1]);					
+					if(b.arrDigits[this.arrDigits.length - i - 1] > 9){												
+						b.arrDigits[this.arrDigits.length - i - 2] = (byte) (this.arrDigits[this.arrDigits.length - i - 2] + 1);
+						b.arrDigits[this.arrDigits.length - i - 1] = (byte) (this.arrDigits[this.arrDigits.length - i - 1]%10);						
 					}
 				}											
 			}else if(d.arrDigits[0] == -1 && this.arrDigits[0] == 0){
-				for (int i = 0; i < this.arrDigits.length - 1; i++) {					
-					if(this.arrDigits[this.arrDigits.length - i - 1] >= d.arrDigits[this.arrDigits.length - i - 1]){
-						this.arrDigits[this.arrDigits.length - i - 1] = (byte)(this.arrDigits[this.arrDigits.length - i - 1] - d.arrDigits[this.arrDigits.length - i - 1]);
-					}else{	
-						this.arrDigits[this.arrDigits.length - i - 2] = (byte) (this.arrDigits[this.arrDigits.length - i - 2] - 1);
-						this.arrDigits[this.arrDigits.length - i - 1] = (byte) ((this.arrDigits[this.arrDigits.length - i - 1] + 10) - d.arrDigits[this.arrDigits.length - i - 1]);
+				byte checkGreater = -1;
+				for (int i = 2; i < this.arrDigits.length; i++) {
+					if(this.arrDigits[i] == d.arrDigits[i])
+						continue;
+					else{
+						if(this.arrDigits[i] > d.arrDigits[i]){
+							checkGreater = 1;
+							break;
+						}else{
+							checkGreater = -1;
+							break;
+						}
 					}
-				}
-			}else if(d.arrDigits[0] == 0 && this.arrDigits[0] == -1){
-				for (int i = 0; i < this.arrDigits.length-1; i++) {					
-					if(this.arrDigits[this.arrDigits.length - i - 1] >= d.arrDigits[this.arrDigits.length - i - 1]){
-						this.arrDigits[this.arrDigits.length - i - 1] = (byte)(this.arrDigits[this.arrDigits.length - i - 1] - d.arrDigits[this.arrDigits.length - i - 1]);
-					}else{	
-						this.arrDigits[this.arrDigits.length - i - 2] = (byte) (this.arrDigits[this.arrDigits.length - i - 2] - 1);
-						this.arrDigits[this.arrDigits.length - i - 1] = (byte) ((this.arrDigits[this.arrDigits.length - i - 1] + 10) - d.arrDigits[this.arrDigits.length - i - 1]);
+				}				
+				if(checkGreater == 1){
+					for (int i = 0; i < this.arrDigits.length-1; i++) {					
+						if(this.arrDigits[this.arrDigits.length - i - 1] >= d.arrDigits[this.arrDigits.length - i - 1]){
+							b.arrDigits[this.arrDigits.length - i - 1] = (byte)(this.arrDigits[this.arrDigits.length - i - 1] - d.arrDigits[this.arrDigits.length - i - 1]);
+						}else{	
+							b.arrDigits[this.arrDigits.length - i - 2] = (byte) (this.arrDigits[this.arrDigits.length - i - 2] - 1);
+							b.arrDigits[this.arrDigits.length - i - 1] = (byte) ((this.arrDigits[this.arrDigits.length - i - 1] + 10) - d.arrDigits[this.arrDigits.length - i - 1]);
+						}
 					}
+				}else if(checkGreater == -1){					
+					for (int i = 0; i < this.arrDigits.length-1; i++) {					
+						if(d.arrDigits[d.arrDigits.length - i - 1] >= this.arrDigits[this.arrDigits.length - i - 1]){
+							d.arrDigits[d.arrDigits.length - i - 1] = (byte)(d.arrDigits[d.arrDigits.length - i - 1] - this.arrDigits[this.arrDigits.length - i - 1]);
+						}else{	
+							System.out.println(d.arrDigits[d.arrDigits.length - i - 2] - 1);
+							d.arrDigits[d.arrDigits.length - i - 2] = (byte) (d.arrDigits[d.arrDigits.length - i - 2] - 1);
+							d.arrDigits[d.arrDigits.length - i - 1] = (byte) ((d.arrDigits[d.arrDigits.length - i - 1] + 10) - this.arrDigits[this.arrDigits.length - i - 1]);
+						}
+					}
+					System.out.println(Arrays.toString(d.arrDigits));
+					System.exit(0);
 				}
+			}else if(d.arrDigits[0] == 0 && this.arrDigits[0] == -1){				
+				
 			}else if(d.arrDigits[0] == -1 && this.arrDigits[0] == -1){				
 				for (int i = 0; i < this.arrDigits.length-1; i++) {
-					this.arrDigits[this.arrDigits.length - i - 1] = (byte)(this.arrDigits[this.arrDigits.length - i - 1] + d.arrDigits[this.arrDigits.length - i - 1]);					
-					if(this.arrDigits[this.arrDigits.length - i - 1] > 9){												
-						this.arrDigits[this.arrDigits.length - i - 2] = (byte) (this.arrDigits[this.arrDigits.length - i - 2] + 1);
-						this.arrDigits[this.arrDigits.length - i - 1] = (byte) (this.arrDigits[this.arrDigits.length - i - 1]%10);						
+					b.arrDigits[this.arrDigits.length - i - 1] = (byte)(this.arrDigits[this.arrDigits.length - i - 1] + d.arrDigits[this.arrDigits.length - i - 1]);					
+					if(b.arrDigits[this.arrDigits.length - i - 1] > 9){												
+						b.arrDigits[this.arrDigits.length - i - 2] = (byte) (this.arrDigits[this.arrDigits.length - i - 2] + 1);
+						b.arrDigits[this.arrDigits.length - i - 1] = (byte) (this.arrDigits[this.arrDigits.length - i - 1]%10);						
 					}
 				}			
-			}	
-//			System.out.println(Arrays.toString(this.arrDigits));
-//			System.exit(0);
-			return this;
+			}
 		}		
-		return null;
+		System.out.println(Arrays.toString(b.arrDigits));
+		System.exit(0);
+		return b;
 	}
 
 	/**
